61769c45cdfa933ceb5d844bab4fa005555316c4
jiyoung-B <catgirl0313@naver.com>
2023-03-14 18:15:28 +0900

    수정중 화이팅

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,12 @@
 import React from "react"
-import Header from '../components/Header';
-import Footer from "../components/Footer";
-import ToIntro from "../components/ToIntro";
-import Nav from "./layout/Nav";
-// import styles from '../styles/Home.module.css'
+
 
 export default function Home() {
   return (
-    <div/* className={styles.container}*/>
+    <div>
         <title>Temfo,</title>
         <h1> Welcome to Temfo, </h1>
-        <Footer />
+
     </div>
   )
 }
--- pages/layout/Nav.js
+++ pages/layout/Nav.js
@@ -1,23 +1,45 @@
-import {Col, Container, NavLink, Row} from "react-bootstrap";
+import {Container, Row, Col, NavLink} from 'react-bootstrap';
 import { HiOutlineMapPin } from 'react-icons/hi2';
-import {BsCalendar} from "react-icons/bs";
-import {CiUser} from "react-icons/ci";
-
+import { BsCalendar } from 'react-icons/bs';
+import { CiUser } from 'react-icons/ci';
+import Link from 'next/link';
 
 const Nav = () => {
     return (
-        <div className={"border-bottom border-2 border-primary"} id={"navWrapper"}>
-            <Container fluid="xxl">
-                <Row className="title">
-                    <Col md={{span: 2}}><NavLink to='/'>Temfo,</NavLink></Col>
-                    <Col md={{span: 1, offset: 3}}><NavLink to='/region'><HiOutlineMapPin />지역</NavLink></Col>
-                    <Col><NavLink to='/calendar'> 일정<BsCalendar /></NavLink></Col>
-                    <Col md={{span:1, offset: 4}}><NavLink to='/login'><CiUser /></NavLink></Col>
+        <div className='border-bottom border-2 border-primary' id='navWrapper'>
+            <Container fluid='xxl'>
+                <Row className='title'>
+                    <Col md={{ span: 2 }}>
+                        <NavLink href='/'>
+                            Temfo
+                        </NavLink>
+                    </Col>
+                    <Col md={{ span: 1, offset: 3 }}>
+                        <Link href='/region'>
+                            <NavLink>
+                                <HiOutlineMapPin />지역
+                            </NavLink>
+                        </Link>
+                    </Col>
+                    <Col>
+                        <Link href='/calendar'>
+                            <NavLink>
+                                일정<BsCalendar />
+                            </NavLink>
+                        </Link>
+                    </Col>
+                    <Col md={{ span: 1, offset: 4 }}>
+                        <NavLink href='/login'>
+                            <NavLink>
+                                <CiUser />
+                            </NavLink>
+                        </NavLink>
+                    </Col>
                 </Row>
             </Container>
-            <div className="navBorder"></div>
+            <div className='navBorder'></div>
         </div>
-    )
-}
+    );
+};
 
 export default Nav;
