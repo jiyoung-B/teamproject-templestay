d4980e6a4c65b5eafc01128c47815d6b65da4553
LeeSeungYeon <masakisqq@naver.com>
2023-03-15 13:13:59 +0900

    좋아요, 내 정보 페이지 추가2

--- pages/myinfo.js
+++ pages/myinfo.js
@@ -15,7 +15,7 @@ export default function Myinfo () {
                     <Col className="infomenu2 col-5">내정보</Col>
                 </Row>
                 <Row className="msg">
-                    <Col className="offset-1">개인정보 유출에 조심하세요.</Col>
+                    <Col className="offset-1">개인정보 유출에 조심하세요</Col>
                 </Row>
                 <Row className="psinfo">
                     <Col className="col-10 offset-1">카드링크</Col>
