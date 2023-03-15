e038fb19045afa244fbce67d783cbbe4dffead29
LeeSeungYeon <masakisqq@naver.com>
2023-03-15 12:58:46 +0900

    좋아요, 내 정보 페이지 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/css/custom.css'
 import '../styles/css/Nav.css'
 import '../styles/globals.css'
+import '../styles/css/MyinfoCommon.css'
+import '../styles/css/likes.css'
+import '../styles/css/myinfo.css'
 import Layout from "./layout/Layout"
 
 
