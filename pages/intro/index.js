a1e828f12b1eabdd0d0ec32549f03ef9dcb34179
jiyoung-B <catgirl0313@naver.com>
2023-03-15 13:28:59 +0900

    복구 - 충일

--- pages/intro/index.js
+++ pages/intro/index.js
@@ -4,34 +4,34 @@ import Footer from "../layout/Footer";
 
 const Intro = () => {
 
-return (
-    <div>
+    return (
         <div>
-            <Link href="/">
-                <div style={{background: "grey"}}>
-                    <h1> Temfo, </h1>
-                    <p>마음챙김, 템포와 함께 떠나보아요.</p>
-                    <img src="/img/sampleImg_1.png" alt="intro_img_1" width={1200} height={400}/>
-                </div>
+            <div>
+                <Link href="/">
+                    <div style={{background: "grey"}}>
+                        <h1> Temfo, </h1>
+                        <p>마음챙김, 템포와 함께 떠나보아요.</p>
+                        <img src="/img/sampleImg_1.png" alt="intro_img_1" width={1200} height={400}/>
+                    </div>
                 </Link>
+            </div>
+            <div>
+                중앙-소개
+                <div><img src="/img/sampleImg_2.png" alt="intro_img_2" width={500} height={400} /></div>
+                <div><img src="/img/sampleImg_2.png" alt="intro_img_2" width={500} height={400} /></div>
+                <div><img src="/img/sampleImg_2.png" alt="intro_img_2" width={500} height={400} /></div>
+                <div><img src="/img/sampleImg_2.png" alt="intro_img_2" width={500} height={400} /></div>
+            </div>
+            <div style={{width:1800}}>
+                하단-에디터픽
+                <ul style={{display: 'inline-block'}}>
+                    <li><Link href="/temple"><img src="/img/sampleImg_2.png" alt="intro_img_2" width={200} height={150} /></Link></li>
+                    <li><Link href="/temple"><img src="/img/sampleImg_2.png" alt="intro_img_2" width={200} height={150} /></Link></li>
+                    <li><Link href="/temple"><img src="/img/sampleImg_2.png" alt="intro_img_2" width={200} height={150} /></Link></li>
+                </ul>
+            </div>
+            <Footer />
         </div>
-        <div>
-            중앙-소개
-            <div><img src="/img/sampleImg_2.png" alt="intro_img_2" width={500} height={400} /></div>
-            <div><img src="/img/sampleImg_2.png" alt="intro_img_2" width={500} height={400} /></div>
-            <div><img src="/img/sampleImg_2.png" alt="intro_img_2" width={500} height={400} /></div>
-            <div><img src="/img/sampleImg_2.png" alt="intro_img_2" width={500} height={400} /></div>
-        </div>
-        <div style={{width:1800}}>
-            하단-에디터픽
-            <ul style={{display: 'inline-block'}}>
-                <li><Link href="/temple"><img src="/img/sampleImg_2.png" alt="intro_img_2" width={200} height={150} /></Link></li>
-                <li><Link href="/temple"><img src="/img/sampleImg_2.png" alt="intro_img_2" width={200} height={150} /></Link></li>
-                <li><Link href="/temple"><img src="/img/sampleImg_2.png" alt="intro_img_2" width={200} height={150} /></Link></li>
-            </ul>
-        </div>
-        <Footer />
-    </div>
 
     )
 }
