109d774ab4147f65e4c0eb4b89f02e033b7d4743
jiyoung-B <catgirl0313@naver.com>
2023-03-16 17:31:17 +0900

    intro 수정, nav 고정

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,59 @@ export default function Home() {
     return (
         <div>
             <title>Temfo,</title>
+            <h1 style={{ paddingTop: "100px" }}> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
+            <h1> Welcome to Temfo, </h1>
             <h1> Welcome to Temfo, </h1>
 
         </div>
     )
 }
+
+
 Home.getLayout = (page) => (
     <Layout meta={{title: 'Temfo, 홈'}}>
+        <>
+        <nav className="fixed-top" style={{ position: "fixed", width: "100%", top: "0", left: "0", right: "0", height: "100px",bottom: "80px" }}>
         <ToIntro />
         <Nav />
+        </nav>
+        </>
         {page}
     </Layout>
 )
