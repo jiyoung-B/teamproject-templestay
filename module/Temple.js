687f039e3db2a63caefcbb12167b48e406110048
KIM CHUNG IL <cnddlf12@naver.com>
2023-03-18 21:49:51 +0900

    temple, program page 새 db 연결 완료 및 다른 프로그램 링크 추가.

--- module/Temple.js
+++ module/Temple.js
@@ -2,15 +2,14 @@ import mariadb from './MariaDB'
 
 // 지금은 like 검색이지만, 프로그램 관련 테이블을 다시 만들때는 꼭! 사찰이름을 넣어야 한다.
 let templeSql ={
-    selectTemple :`select * from TEMPLE where T_NAME = ?`,
-    selectTemplePic : `select * from TEMPLEPIC where T_NAME = ?`,
-    selectTempleProPic : `select * from PROGRAMPIC where P_NAME LIKE ?`
+    selectTemple :`select * from TEMPLE2 where T_NAME = ?`,
+    selectTemplePic : `select * from TEMPLEPIC2 where T_NAME = ?`,
+    selectTempleProPic : `select * from PROGRAMPIC2 where T_NAME = ?`
 }
 
 async function selectTemple(id) {  // 템플 정보 출력
     let conn = null;
     let params = [id];
-    let likeParams = ['%['+id+']%']
     let templeData = ''; // 결과 저장용
     let templePicData = '';
     let templeProPicData = '';
@@ -20,7 +19,7 @@ async function selectTemple(id) {  // 템플 정보 출력
 
         templeData = await conn.query(templeSql.selectTemple, params);
         templePicData = await conn.query(templeSql.selectTemplePic, params);
-        templeProPicData = await conn.query(templeSql.selectTempleProPic,likeParams)
+        templeProPicData = await conn.query(templeSql.selectTempleProPic,params)
 
 
     } catch (e) {
