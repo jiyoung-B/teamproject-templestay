1af918271b471299a5eb37f7831b8ef85f5e1c7c
KIM CHUNG IL <cnddlf12@naver.com>
2023-03-20 14:18:30 +0900

    program page 날짜 선택 추가, 및 유틸리티에 날짜 포멧 변경 함수 추가  milliFomatter : 밀리초 => 2023-03-21,  dateFomatter : Tue Mar 21 2023 13:40:14 => 2023-03-21

--- pages/index.js
+++ pages/index.js
@@ -10,78 +10,78 @@ import axios from "axios";
 import * as PropTypes from "prop-types";
 
 export async function getServerSideProps(ctx) {
-        let {lid = '서울'} = ctx.query
+    let {lid = '서울'} = ctx.query
 
 
 
-        let param = `?lid=${lid}`
-        let url = `http://localhost:3000/api/${param}`
+    let param = `?lid=${lid}`
+    let url = `http://localhost:3000/api/${param}`
 
-        const res = await axios.get(url)
-        let indexInfo = res.data
+    const res = await axios.get(url)
+    let indexInfo = res.data
 
 
 
-        return {props:{indexInfo}}
+    return {props:{indexInfo}}
 
 }
 
 export default function Home({indexInfo}) {
     return (
         <div className="bg-white" id="wrapper">
-                <Container fluid>
-                        <Row style={{height: '100px'}}></Row>
-                        <Row className="likeslist tpl align-top">
-                                <Col>
-                                        { (indexInfo.length > 0 ) ? (      indexInfo.map(program => (
+            <Container fluid>
+                <Row style={{height: '100px'}}></Row>
+                <Row className="likeslist tpl align-top">
+                    <Col>
+                        { (indexInfo.length > 0 ) ? (      indexInfo.map(program => (
 
-                                                <Link href={`/temple?id=${program.T_NAME}&pid=${program.PID}`}>
-                                                        <Row className="tpl border border-2 border-danger rounded-2" style={{height: '150px'}}>
-                                                                <Col>
-                                                                        <img src={program.P_PICLINK} alt="프로그램 이미지" className={""} style={{width: '100%', height:'135px',paddingTop:'9px'}}/>
-                                                                </Col>
-                                                                <Col>
-                                                                        <p className={"text-center fs-6"}>{program.ADDR}</p>
-                                                                </Col>
-                                                                <Col>
-                                                                        <p className={"text-center  fs-6"}>{program.P_STRDATE} ~ {program.P_ENDDATE}</p>
-                                                                </Col>
-                                                                <Col>
-                                                                        <p className={"text-center  fs-6"}>{program.P_NAME}</p>
-                                                                </Col>
-                                                        </Row>
-                                                </Link>
+                                <Link href={`/temple?id=${program.T_NAME}&pid=${program.PID}`}>
+                                    <Row className="tpl border border-2 border-danger rounded-2" style={{height: '150px'}}>
+                                        <Col>
+                                            <img src={program.P_PICLINK} alt="프로그램 이미지" className={""} style={{width: '100%', height:'135px',paddingTop:'9px'}}/>
+                                        </Col>
+                                        <Col>
+                                            <p className={"text-center fs-6"}>{program.ADDR}</p>
+                                        </Col>
+                                        <Col>
+                                            <p className={"text-center  fs-6"}>{program.P_STRDATE} ~ {program.P_ENDDATE}</p>
+                                        </Col>
+                                        <Col>
+                                            <p className={"text-center  fs-6"}>{program.P_NAME}</p>
+                                        </Col>
+                                    </Row>
+                                </Link>
 
-                                            )
-                                        )) : (
-                                                    <Row className="tpl border border-2 border-danger rounded-2" style={{height: '150px'}}>
-                                                           <p className={'h1'}>프로그램이 없습니다.</p>
-                                                    </Row>
-                                        )
-                                          }
-                                </Col>
-                                <Col>
-                                        <Row>
-                                                <Link href={"/?lid=인천"}>인천</Link>
-                                                <Link href={"/?lid=서울"}>서울</Link>
-                                                <Link href={"/?lid=강원"}>강원</Link>
-                                                <Link href={"/?lid=충남"}>충남</Link>
-                                                <Link href={"/?lid=경기"}>경기</Link>
-                                                <Link href={"/?lid=충북"}>충북</Link>
-                                                <Link href={"/?lid=세종"}>세종</Link>
-                                                <Link href={"/?lid=경북"}>경북</Link>
-                                                <Link href={"/?lid=전북"}>전북</Link>
-                                                <Link href={"/?lid=대구"}>대구</Link>
-                                                <Link href={"/?lid=인천"}>광주</Link>
-                                                <Link href={"/?lid=광주"}>전남</Link>
-                                                <Link href={"/?lid=경남"}>경남</Link>
-                                                <Link href={"/?lid=부산"}>부산</Link>
-                                                <Link href={"/?lid=제주"}>제주</Link>
-                                        </Row>
-                                </Col>
+                            )
+                        )) : (
+                            <Row className="tpl border border-2 border-danger rounded-2" style={{height: '150px'}}>
+                                <p className={'h1'}>프로그램이 없습니다.</p>
+                            </Row>
+                        )
+                        }
+                    </Col>
+                    <Col>
+                        <Row>
+                            <Link href={"/?lid=인천"}>인천</Link>
+                            <Link href={"/?lid=서울"}>서울</Link>
+                            <Link href={"/?lid=강원"}>강원</Link>
+                            <Link href={"/?lid=충남"}>충남</Link>
+                            <Link href={"/?lid=경기"}>경기</Link>
+                            <Link href={"/?lid=충북"}>충북</Link>
+                            <Link href={"/?lid=세종"}>세종</Link>
+                            <Link href={"/?lid=경북"}>경북</Link>
+                            <Link href={"/?lid=전북"}>전북</Link>
+                            <Link href={"/?lid=대구"}>대구</Link>
+                            <Link href={"/?lid=인천"}>광주</Link>
+                            <Link href={"/?lid=광주"}>전남</Link>
+                            <Link href={"/?lid=경남"}>경남</Link>
+                            <Link href={"/?lid=부산"}>부산</Link>
+                            <Link href={"/?lid=제주"}>제주</Link>
                         </Row>
+                    </Col>
+                </Row>
 
-                </Container>
+            </Container>
 
         </div>
     )
