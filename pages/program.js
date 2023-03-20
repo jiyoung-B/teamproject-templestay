e76ed6570051fac53e1abc400f0287680c32ccf8
KIM CHUNG IL <cnddlf12@naver.com>
2023-03-20 14:33:19 +0900

    program page 날짜 선택 추가, 및 유틸리티에 날짜 포멧 변경 함수 추가  milliFomatter : 밀리초 => 2023-03-21,  dateFomatter : Tue Mar 21 2023 13:40:14 => 2023-03-21

--- pages/program.js
+++ pages/program.js
@@ -1,11 +1,15 @@
 import Carousel from "react-bootstrap/Carousel";
-import {Button, Card, Col, Container, NavLink, Row, Table} from "react-bootstrap";
+import {Button, Card, Col, Container, Modal, NavLink, Row, Table} from "react-bootstrap";
 import {BsCheck2} from "react-icons/bs";
 import axios from "axios";
 import Layout from "./layout/Layout";
 import Nav from "./layout/Nav";
 import shortid from 'shortid'
-import {handleImgError} from "../components/Util";
+import {handleImgError, milliFomatter, dateFomatter} from "../components/Util";
+import {useState} from "react";
+import DatePicker from "react-datepicker";
+import {ko} from "date-fns/locale";
+import "react-datepicker/dist/react-datepicker.css";
 //shortid.generate()
 
 
@@ -26,6 +30,27 @@ export async function getServerSideProps(ctx) {
 export default function Program ({proData}) {
     const unit = 28
 
+    // 내일의 날짜를 구하기
+    const tomorrow = new Date().setDate(new Date().getDate() + 1);
+
+
+    // state 보여주는 부분
+    const [show, setShow] = useState(false);
+    const [startDate, setStartDate] = useState(tomorrow);
+    const [endDate, setEndDate] = useState(null);
+
+    // 모달 on/off 해주는 함수
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    //날짜가 선택되면 state를 변경해주는 함수
+    const onChange = (dates) => {
+        const [start, end] = dates;
+        setStartDate(start);
+        setEndDate(end);
+    };
+
+
     return(
         <div className={'container'} style={{marginTop:`${unit*2}px`}} id={'programWrapper'}>
 
@@ -38,17 +63,17 @@ export default function Program ({proData}) {
                     <Carousel>
 
                         { (proData[1].length > 0) ? (proData[1].map(pic => (
-                            <Carousel.Item key={shortid.generate()}>
-                                <img
-                                    className="d-block w-100"
-                                    src={pic}
-                                    alt="First slide"
-                                    height="800px"
-                                    onError={handleImgError}
-                                    key={shortid.generate()}
-                                />
-                            </Carousel.Item>
-                        ))) :
+                                <Carousel.Item key={shortid.generate()}>
+                                    <img
+                                        className="d-block w-100"
+                                        src={pic}
+                                        alt="First slide"
+                                        height="800px"
+                                        onError={handleImgError}
+                                        key={shortid.generate()}
+                                    />
+                                </Carousel.Item>
+                            ))) :
                             (
                                 <Carousel.Item key={shortid.generate()}>
                                     <img
@@ -61,7 +86,7 @@ export default function Program ({proData}) {
                                     />
                                 </Carousel.Item>
                             )
-                            }
+                        }
                     </Carousel>
 
                 </div>
@@ -73,7 +98,40 @@ export default function Program ({proData}) {
                         <Col lg={4}>
                             <Container>
                                 <Row>
-                                    <Col><p className={'text-center m-0 fs-3 text-primary'}>일정 <BsCheck2 className={"mb-2"}/></p></Col>
+                                    <Col>
+
+                                        <>
+                                            <Button className="calbtn" onClick={handleShow}><p className={'text-center m-0 fs-3 text-primary'}>일정 <BsCheck2 className={"mb-2"}/></p></Button>
+                                            <Modal show={show} onHide={handleClose}>
+                                                <Modal.Header style={{justifyContent: "center", height: "45px", color: "#331904"}}>
+                                                    <Modal.Title>날짜 선택</Modal.Title>
+                                                </Modal.Header>
+                                                <Modal.Body className="cal">
+                                                    <DatePicker
+                                                        selected={startDate}
+                                                        onChange={onChange}
+                                                        inline
+                                                        startDate={startDate}
+                                                        endDate={endDate}
+                                                        minDate={tomorrow}
+                                                        monthsShown={2}
+                                                        selectsRange
+                                                        dateFormat="yyyy-mm-dd"
+                                                        locale={ko}
+                                                    />
+                                                </Modal.Body>
+                                                <Modal.Footer>
+                                                    <Button variant="secondary" onClick={handleClose} style={{backgroundColor: "#331904"}}>
+                                                        닫기
+                                                    </Button>
+                                                    <Button variant="primary" onClick={handleClose}>
+                                                        선택
+                                                    </Button>
+                                                </Modal.Footer>
+                                            </Modal>
+                                        </>
+
+                                    </Col>
                                     <Col><p className={'text-center m-0 fs-3 text-primary'}>인원 <BsCheck2 className={"mb-2"}/></p></Col>
                                 </Row>
                             </Container>
@@ -86,7 +144,9 @@ export default function Program ({proData}) {
 
                 <Container>
                     <Row>
-                        <Col><p className={'text-end pt-2 m-0'}>2023.03.02~2023.03.04</p></Col>
+                        <Col>
+                            { dateFomatter(endDate) ? (<p className={'text-end pt-2 m-0'}>{dateFomatter(startDate)}~{dateFomatter(endDate)}</p>):(<p className={'text-end m-0'}>{dateFomatter(startDate)}</p>)}
+                        </Col>
                         <Col>
                             <Container>
                                 <Row>
@@ -145,34 +205,34 @@ export default function Program ({proData}) {
                             <p>기타 코멘트</p>
                         </Col>
                         <Col md={9}>
-                        {proData[4].map(day => (
-                            <Row key={shortid.generate()}>
+                            {proData[4].map(day => (
+                                    <Row key={shortid.generate()}>
 
-                                <Col key={shortid.generate()}>
-                                    <p className={'fs-5 fw-bold'} key={shortid.generate()}>{day.P_DAY}</p>
+                                        <Col key={shortid.generate()}>
+                                            <p className={'fs-5 fw-bold'} key={shortid.generate()}>{day.P_DAY}</p>
 
-                                    <Table key={shortid.generate()}>
-                                        <thead key={shortid.generate()}>
-                                        <tr key={shortid.generate()}>
-                                            <th style={{width:'50%'}} key={shortid.generate()}>시작시간</th>
-                                            <th key={shortid.generate()}>일정명</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody key={shortid.generate()}>
+                                            <Table key={shortid.generate()}>
+                                                <thead key={shortid.generate()}>
+                                                <tr key={shortid.generate()}>
+                                                    <th style={{width:'50%'}} key={shortid.generate()}>시작시간</th>
+                                                    <th key={shortid.generate()}>일정명</th>
+                                                </tr>
+                                                </thead>
+                                                <tbody key={shortid.generate()}>
 
-                                        {day.P_INFO.map(sch => (
-                                            <tr key={shortid.generate()}>
-                                                <td style={{width:'50%'}} key={shortid.generate()}>{sch.P_TIME}</td>
-                                                <td key={shortid.generate()}>{sch.P_CONTENT}</td>
-                                            </tr>
-                                        ))}
+                                                {day.P_INFO.map(sch => (
+                                                    <tr key={shortid.generate()}>
+                                                        <td style={{width:'50%'}} key={shortid.generate()}>{sch.P_TIME}</td>
+                                                        <td key={shortid.generate()}>{sch.P_CONTENT}</td>
+                                                    </tr>
+                                                ))}
 
-                                        </tbody>
-                                    </Table>
-                                </Col>
-                            </Row>
-                        )
-                        )}
+                                                </tbody>
+                                            </Table>
+                                        </Col>
+                                    </Row>
+                                )
+                            )}
                         </Col>
                     </Row>
                 </Container>
@@ -199,18 +259,8 @@ export default function Program ({proData}) {
 
                             ))) : ( <Col md={4} style={{ marginTop:`${unit}px`, flexBasis: '432px' }} key={shortid.generate()}>
 
-                                <Card style={{ width: '100%' }} key={shortid.generate()}>
-                                    <Card.Img variant="top" src={'https://www.templestay.com/images/templeinfo-00.jpg'} onError={handleImgError} style={{height: '280px'}} key={shortid.generate()}/>
-                                    <Card.Body key={shortid.generate()}>
-                                        <Card.Title style={{height:`70px`}} key={shortid.generate()}>
-                                            {program.P_NAME}
-                                        </Card.Title>
-                                        <Button variant="primary" key={shortid.generate()}><NavLink href={`/program?pid=${program.PID}`} key={shortid.generate()}>예약하러 가기</NavLink></Button>
-                                    </Card.Body>
-                                </Card>
-
                             </Col>)
-                                }
+                            }
                         </Row>
                     </Container>
                 </div>
