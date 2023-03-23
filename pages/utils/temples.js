49c658464e3779814387f1755364f782535ddf29
LeeSeungYeon <masakisqq@naver.com>
2023-03-23 18:54:07 +0900

    비교하기 창 지도 추가 (아직 수정 중)!!

--- pages/utils/temples.js
+++ pages/utils/temples.js
@@ -1,56 +1,68 @@
 export const temples = [
     {
         name: "11사",
-        location: "장소1",
+        location: "장소1 - 합정 메세나폴리스",
         day: "날짜1",
         program: "프로그램1",
         number: 1,
         price: 10000,
-        details: "상세정보1"
+        details: "상세정보1",
+        lat: 37.55093934971679,
+        lng: 126.91376993239895
     },
     {
         name: "22사",
-        location: "장소2",
+        location: '장소2 - 남한 최북단',
         day: '날짜2',
         program: '프로그램2',
         number: 2,
         price: 20000,
-        details: "상세정보2"
+        details: "상세정보2",
+        lat: 38.61232872301094,
+        lng: 128.35530930033244
     },
     {
         name: "33사",
-        location: '장소3',
+        location: '장소3 - 평창 버스터미널',
         day: '날짜3',
         program: '프로그램3',
         number: 3,
         price: 30000,
-        details: "상세정보3"
+        details: "상세정보3",
+        lat: 37.36645283866851,
+        lng: 128.3933480829004
     },
     {
         name: "44사",
-        location: '장소4',
+        location: '장소4 - 경북대학교 대구캠퍼스',
         day: '날짜4',
         program: '프로그램4',
         number: 4,
         price: 40000,
-        details: "상세정보4"
+        details: "상세정보4",
+        lat: 35.89067504759844,
+        lng: 128.6122075051451
     },
     {
         name: "55사",
-        location: '장소5',
+        location: '장소5 - 세종시 시청',
         day: '날짜5',
         program: '프로그램5',
         number: 5,
         price: 50000,
-        details: "상세정보5"
+        details: "상세정보5",
+        lat: 36.4800722828353,
+        lng: 127.2891283985598
     },
     {
         name: "66사",
-        location: '장소6',
+        location: '장소6 - 제주 공항',
         day: '날짜6',
         program: '프로그램6',
         number: 6,
         price: 60000,
-        details: "상세정보6"
+        details: "상세정보6",
+        lat: 33.507143500555294,
+        lng: 126.4926782238023
     }
 ];
