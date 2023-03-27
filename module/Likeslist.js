d445396cf83c933127fcbd3a791b978d294a0a39
LeeSeungYeon <masakisqq@naver.com>
2023-03-27 18:35:30 +0900

    좋아요 페이지 수정 중!

--- module/Likeslist.js
+++ module/Likeslist.js
@@ -1,35 +1,40 @@
 import mariadb from './MariaDB';
 
 let likeslistsql = {
-    // selectTemple : ' select T_NAME from LIKES join PROGRAM3 using (PID) where email = ? ',
-    // selectProgram : ' select P_NAME from LIKES join PROGRAM3 using (PID) where email = ?',
-    // selectAddr: ` select ADDR from LIKES join PROGRAM3 using (PID) join TEMPLE3 using (T_NAME) where email = ? `,
-    // selectCount: ` select count(LK_NO) cnt from LIKES where email = ? `
-    selectLikeslist : ` select T_NAME, P_NAME, ADDR from LIKES join PROGRAM3 using (PID) join TEMPLE3 using (T_NAME)
-                        where email = ? `
+    // selectLikeslist : ` select PID, T_NAME, P_NAME, ADDR from LIKES join PROGRAM3 using (PID) join TEMPLE3 using (T_NAME)
+    //                     where email = ? `,
+    selectLikeslist : ` select l.PID, T_NAME, P_NAME, ADDR, PR_CLASS, PRICE, P_DAY, P_TIME, P_CONTENT
+                        from LIKES l join PROGRAM3 using (PID)
+                                     join TEMPLE3 using (T_NAME)
+                                     join PROGRAMPRICE3 using (P_NAME)
+                                     join PROGRAMSCHEDULE3 using (P_NAME)
+                        where email = ? group by l.PID `,
+//     selectCompare : ` select distinct l.PID, T_NAME, p.P_NAME, ADDR, PRICE, P_CONTENT
+// from LIKES l join PROGRAM3 p using (PID)
+//     join PROGRAMSCHEDULE3 s using (PID)
+// join PROGRAMPRICE3 using (PID)
+// join TEMPLE3 using (T_NAME)
+// where (email = ?)
+// order by PID; `
 }
 
 // class Likeslist {
-//     constructor(T_NAME, P_NAME, ADDR, LK_NO) {
+//     constructor(T_NAME, P_NAME, ADDR, PRICE, P_CONTENT) {
 //         this.T_NAME = T_NAME;
 //         this.P_NAME = P_NAME;
 //         this.ADDR = ADDR;
-//         this.LK_NO = LK_NO;
+//         this.PRICE = PRICE;
+//         this.P_CONTENT = P_CONTENT;
 //     }
 
     async function selectLikeslist(email) {
         let conn = null;
         let params = [email];
-        // let result = '';
         let likeslist = [];   // 결과 저장용
 
         try {
             conn = await mariadb.makeConn();
             let likes = await conn.query(likeslistsql.selectLikeslist, params);
-
-            // 조회수 증가 코드
-            // await conn.query(boardsql.viewOne, params);
-            // await conn.commit();
 
             likeslist.push(likes)
 
@@ -38,27 +43,35 @@ let likeslistsql = {
         } finally {
             await mariadb.closeConn();
         }
-        console.log(`plz: `, likeslist)
+        console.log('왜', likeslist)
 
         return likeslist;
     }
 
-    export default selectLikeslist
+    // export default selectLikeslist
 
-//     async selectCount(email) {
-//         let conn = null;
-//         let params = [email];
-//         let result = '';
-//
-//         try {
-//             conn = await mariadb.makeConn();
-//             result = await conn.query(likeslistsql.selectCount, params);
-//         } catch (e) {
-//             console.log(e);
-//         } finally {
-//             await mariadb.closeConn();
-//         }
-//
-//         return result;
-//     }
+    // async selectCompare(email) {
+    //     let conn = null;
+    //     let params = [email];
+    //     let comparelist = [];   // 결과 저장용
+    //
+    //     try {
+    //         conn = await mariadb.makeConn();
+    //         let comps = await conn.query(likeslistsql.selectCompare, params);
+    //
+    //         comparelist.push(comps)
+    //
+    //     } catch (e) {
+    //         console.log(e);
+    //     } finally {
+    //         await mariadb.closeConn();
+    //     }
+    //
+    //     return comparelist;
+    // }
 // }
+
+export default selectLikeslist
+
+// export default Likeslist
+// module.exports = Likeslist;
