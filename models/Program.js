c58a20e944ec0990bc0e03c0d610cb4775c19e8b
KIM CHUNG IL <cnddlf12@naver.com>
2023-03-28 18:45:29 +0900

    distinctLikeList3

--- models/Program.js
+++ models/Program.js
@@ -1,11 +1,14 @@
 import mariadb from './MariaDB'
+import {milliFomatter} from "./Utils";
 let programSql ={
-    selectPro :` select P_NAME,T_NAME,P_CAUTION,P_STRDATE,P_ENDDATE,P_INTRO, P_LINK,P_CLASS from PROGRAM2 where PID = ?`,
+    selectPro :` select P_NAME,T_NAME,P_CAUTION,P_STRDATE,P_ENDDATE,P_INTRO, P_LINK,P_CLASS from PROGRAM2 where PID = ? and P_ENDDATE > ?`,
     selectProPic : `select * from PROGRAMPIC2 where T_NAME = ?`,
     selectProPrice : `select DIVISION,PR_CLASS,PRICE from PROGRAMPRICE2 where PID = ? order by PR_NO`,
     selectProSchedule : ` select P_DAY, P_TIME,P_CONTENT from PROGRAMSCHEDULE2 where PID = ? ORDER BY PS_NO `,
     selectProDes : ` select P_DES, P_DETAIL from PROGRAMDES2 WHERE PID = ? ORDER BY PD_NO `
 }
+
+let tomorrow = milliFomatter(new Date().setDate(new Date().getDate() + 1));
 async function selectPro(pid) {
     let conn = null;
 
@@ -14,7 +17,7 @@ async function selectPro(pid) {
     try {
         conn = await mariadb.makeConn();
 
-        let paramPid = [pid];
+        let paramPid = [pid,tomorrow];
         let P_Meta = await conn.query(programSql.selectPro, paramPid);
 
         let paramTName = [await P_Meta[0].T_NAME]
