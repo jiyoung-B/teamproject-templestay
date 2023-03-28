d82e67263d22d3c26e8e2a8d13da61b44e2f6cdb
KIM CHUNG IL <cnddlf12@naver.com>
2023-03-28 23:50:20 +0900

    비교 페이지 스타일 일단락

--- models/Index.js
+++ models/Index.js
@@ -27,6 +27,12 @@ from TEMPLE2 t
          INNER JOIN PROGRAMPIC2 pp on p.P_NAME = pp.P_NAME
 WHERE (p.P_STRDATE < ? AND p.P_ENDDATE > ?) ORDER by REVIEWCNT DESC `
 
+let selectDefault = ` select p.P_NAME,p.PID,t.T_NAME,ADDR,P_CLASS, date_format(P_STRDATE, '%Y-%m-%d') P_STRDATE, date_format(P_ENDDATE, '%Y-%m-%d') P_ENDDATE,P_PICLINK, pp.E_PICKTF,t.REGION
+                      from TEMPLE2 t
+                               INNER JOIN PROGRAM2 p ON t.T_NAME = p.T_NAME
+                               INNER JOIN PROGRAMPIC2 pp on p.P_NAME = pp.P_NAME
+                      where p.P_ENDDATE > ?`
+
 
 let tomorrow = milliFomatter(new Date().setDate(new Date().getDate() + 1));
 
@@ -41,7 +47,7 @@ class Index {
     }
 
 
-    async editorPic () { // 검사 완
+    async default () { // 검사 완
         let conn;
         let defaultData
 
@@ -49,7 +55,7 @@ class Index {
             conn = await mariadb.makeConn();
             let param = [tomorrow]
 
-            defaultData = await conn.query(selectEditorPicSql,param)
+            defaultData = await conn.query(selectDefault,param)
 
         } catch(e) {
             console.log(e)
