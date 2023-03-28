54aa6b39978e975d58d6960f4c94aeeba7649dd9
KIM CHUNG IL <cnddlf12@naver.com>
2023-03-28 11:01:22 +0900

    리뷰 수에 따라 프로그램 옆에 별이 등장함 2000+ 별3 1000+ 별2 500+ 별1

--- models/Index.js
+++ models/Index.js
@@ -2,29 +2,30 @@ import mariadb from './MariaDB'
 import {milliFomatter} from "./Utils";
 
 let localSql =
-    ` select p.P_NAME,p.PID,t.T_NAME,ADDR,P_CLASS, date_format(P_STRDATE, '%Y-%m-%d') P_STRDATE, date_format(P_ENDDATE, '%Y-%m-%d') P_ENDDATE,P_PICLINK
+    ` select p.P_NAME,p.PID,t.T_NAME,ADDR,P_CLASS, date_format(P_STRDATE, '%Y-%m-%d') P_STRDATE, date_format(P_ENDDATE, '%Y-%m-%d') P_ENDDATE,P_PICLINK,t.REVIEWCNT
 from TEMPLE2 t
 INNER JOIN PROGRAM2 p ON t.T_NAME = p.T_NAME
 INNER JOIN PROGRAMPIC2 pp on p.P_NAME = pp.P_NAME
-WHERE t.REGION = ? and p.P_ENDDATE > ?`
+WHERE t.REGION = ? and p.P_ENDDATE > ?
+ORDER by REVIEWCNT DESC `
 
-let selectEditorPicSql = ` select p.P_NAME,p.PID,t.T_NAME,ADDR,P_CLASS, date_format(P_STRDATE, '%Y-%m-%d') P_STRDATE, date_format(P_ENDDATE, '%Y-%m-%d') P_ENDDATE,P_PICLINK, pp.E_PICKTF,t.REGION
+let selectEditorPicSql = ` select p.P_NAME,p.PID,t.T_NAME,ADDR,P_CLASS, date_format(P_STRDATE, '%Y-%m-%d') P_STRDATE, date_format(P_ENDDATE, '%Y-%m-%d') P_ENDDATE,P_PICLINK, pp.E_PICKTF,t.REGION,t.REVIEWCNT
 from TEMPLE2 t
          INNER JOIN PROGRAM2 p ON t.T_NAME = p.T_NAME
          INNER JOIN PROGRAMPIC2 pp on p.P_NAME = pp.P_NAME
-where E_PICKTF = 1 and p.P_ENDDATE > ? `;
+where E_PICKTF = 1 and p.P_ENDDATE > ? ORDER by REVIEWCNT DESC `;
 
-let selectLNDateSql = ` select p.P_NAME,p.PID,t.T_NAME,ADDR,P_CLASS, date_format(P_STRDATE, '%Y-%m-%d') P_STRDATE, date_format(P_ENDDATE, '%Y-%m-%d') P_ENDDATE,P_PICLINK, pp.E_PICKTF, t.REGION
+let selectLNDateSql = ` select p.P_NAME,p.PID,t.T_NAME,ADDR,P_CLASS, date_format(P_STRDATE, '%Y-%m-%d') P_STRDATE, date_format(P_ENDDATE, '%Y-%m-%d') P_ENDDATE,P_PICLINK, pp.E_PICKTF, t.REGION,t.REVIEWCNT
                         from TEMPLE2 t
                                  INNER JOIN PROGRAM2 p ON t.T_NAME = p.T_NAME
                                  INNER JOIN PROGRAMPIC2 pp on p.P_NAME = pp.P_NAME
-                        WHERE (t.REGION = ?) and (p.P_STRDATE < ? AND p.P_ENDDATE > ?) `;
+                        WHERE (t.REGION = ?) and (p.P_STRDATE < ? AND p.P_ENDDATE > ?) ORDER by REVIEWCNT DESC `;
 
-let selectDateSql = ` select p.P_NAME,p.PID,t.T_NAME,ADDR,P_CLASS, date_format(P_STRDATE, '%Y-%m-%d') P_STRDATE, date_format(P_ENDDATE, '%Y-%m-%d') P_ENDDATE,P_PICLINK, pp.E_PICKTF, t.REGION
+let selectDateSql = ` select p.P_NAME,p.PID,t.T_NAME,ADDR,P_CLASS, date_format(P_STRDATE, '%Y-%m-%d') P_STRDATE, date_format(P_ENDDATE, '%Y-%m-%d') P_ENDDATE,P_PICLINK, pp.E_PICKTF, t.REGION,t.REVIEWCNT
 from TEMPLE2 t
          INNER JOIN PROGRAM2 p ON t.T_NAME = p.T_NAME
          INNER JOIN PROGRAMPIC2 pp on p.P_NAME = pp.P_NAME
-WHERE (p.P_STRDATE < ? AND p.P_ENDDATE > ?) `
+WHERE (p.P_STRDATE < ? AND p.P_ENDDATE > ?) ORDER by REVIEWCNT DESC `
 
 
 let tomorrow = milliFomatter(new Date().setDate(new Date().getDate() + 1));
